e92374007487c86da2495b9ad6b81ae1810b4df0
Dmitry Gordeev <netimen@gmail.com>
2016-03-20 13:25:39 +0300

    move kotlin dependency to top-level build.gradle

--- app/src/main/java/ru/netimen/hitch_hikingstats/Go.kt
+++ app/src/main/java/ru/netimen/hitch_hikingstats/Go.kt
@@ -13,17 +13,17 @@ import com.trello.rxlifecycle.RxLifecycle
 import org.jetbrains.anko.*
 import org.jetbrains.anko.support.v4.UI
 import org.jetbrains.anko.support.v4.onUiThread
-import ru.netimen.hitch_hikingstats.lib.*
+import ru.netimen.hitch_hikingstats.lib.LoadObservable
+import ru.netimen.hitch_hikingstats.lib.MvpFragment
+import ru.netimen.hitch_hikingstats.presentation.GoLogic
+import ru.netimen.hitch_hikingstats.presentation.GoPresenter
+import ru.netimen.hitch_hikingstats.presentation.GoView
 import rx.Observable
-import rx.Subscription
-import rx.schedulers.Schedulers
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.InjektMain
 import uy.kohesive.injekt.api.InjektRegistrar
 import uy.kohesive.injekt.api.fullType
 import uy.kohesive.injekt.api.get
-import java.util.concurrent.TimeUnit
-import kotlin.properties.Delegates
 
 /**
  * Copyright (c) 2016 Bookmate.
@@ -32,53 +32,6 @@ import kotlin.properties.Delegates
  * Author: Dmitry Gordeev <netimen@dreamindustries.co>
  * Date:   03.03.16
  */
-
-interface GoView : MvpView {
-    fun rideClicked(): Observable<Unit>
-    fun stopClicked(): Observable<Unit>
-    fun waitClicked(): Observable<Unit>
-    fun carSelected(): Observable<String>
-
-    fun showState(state: GoState)
-    fun updateTitle(state: GoState)
-}
-
-interface GoLogic {
-    fun loadState(): LoadObservable<GoState, ErrorInfo>
-    fun saveState(state: GoState)
-
-    fun addRide(state: GoState)
-}
-
-// cur notification
-class GoPresenter(view: GoView, val logic: GoLogic) : Presenter<GoView>(view) {
-    private var state by Delegates.countedObservable<GoState>(GoState.Idle()) { prop, old, new, setCount ->
-        if (setCount > 0)
-            logic.saveState(new)
-
-        onStateUpdated(new)
-    }
-    private var updateTitleSubscription: Subscription? = null
-
-    init {
-        logic.loadState().onData { state = it }.subscribe() // cur lifecycle here
-
-        view.stopClicked().bindToLifecycle().subscribe {
-            logic.addRide(state)
-            state = GoState.Idle()
-        }
-        view.waitClicked().bindToLifecycle().subscribe { state = GoState.Waiting() }
-        view.rideClicked().bindToLifecycle().subscribe { state = GoState.Riding("Toyota", state.lengthMinutes) }//CUR: get car
-    }
-
-    private fun onStateUpdated(newState: GoState) {
-        updateTitleSubscription?.unsubscribe()
-        updateTitleSubscription = Observable.interval(1, TimeUnit.MINUTES).bindToLifecycle().subscribe { view.updateTitle(state) }
-
-        view.showState(newState)
-    }
-
-}
 
 class GoFragment : MvpFragment<GoPresenter, GoFragment>(), GoView {
     private val ui = GoFragmentUI()
---
+++ presentation/src/main/java/ru/netimen/hitch_hikingstats/presentation/Go.kt
@@ -0,0 +1,62 @@
+package ru.netimen.hitch_hikingstats.presentation
+
+import rx.Observable
+import rx.Subscription
+import java.util.concurrent.TimeUnit
+import kotlin.properties.Delegates
+
+/**
+ * Copyright (c) 2016 Bookmate.
+ * All Rights Reserved.
+ *
+ * Author: Dmitry Gordeev <netimen@dreamindustries.co>
+ * Date:   20.03.16
+ */
+
+interface GoView : MvpView {
+    fun rideClicked(): Observable<Unit>
+    fun stopClicked(): Observable<Unit>
+    fun waitClicked(): Observable<Unit>
+    fun carSelected(): Observable<String>
+
+    fun showState(state: GoState)
+    fun updateTitle(state: GoState)
+}
+
+interface GoLogic {
+    fun loadState(): LoadObservable<GoState, ErrorInfo>
+    fun saveState(state: GoState)
+
+    fun addRide(state: GoState)
+}
+
+// cur notification
+class GoPresenter(view: GoView, val logic: GoLogic) : Presenter<GoView>(view) {
+    private var state by Delegates.countedObservable<GoState>(GoState.Idle()) { prop, old, new, setCount ->
+        if (setCount > 0)
+            logic.saveState(new)
+
+        onStateUpdated(new)
+    }
+    private var updateTitleSubscription: Subscription? = null
+
+    init {
+        logic.loadState().onData { state = it }.subscribe() // cur lifecycle here
+
+        view.stopClicked().bindToLifecycle().subscribe {
+            logic.addRide(state)
+            state = GoState.Idle()
+        }
+        view.waitClicked().bindToLifecycle().subscribe { state = GoState.Waiting() }
+        view.rideClicked().bindToLifecycle().subscribe { state = GoState.Riding("Toyota", state.lengthMinutes) }//CUR: get car
+    }
+
+    private fun onStateUpdated(newState: GoState) {
+        updateTitleSubscription?.unsubscribe()
+        updateTitleSubscription = Observable.interval(1, TimeUnit.MINUTES).bindToLifecycle().subscribe { view.updateTitle(state) }
+
+        view.showState(newState)
+    }
+
+}
+
