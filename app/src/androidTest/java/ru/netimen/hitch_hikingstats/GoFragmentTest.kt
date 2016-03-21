713302379d05a9e91047993029db89d0c63acd0a
Dmitry Gordeev <netimen@gmail.com>
2016-03-21 16:06:38 +0300

    test state displayed correctly

--- app/src/androidTest/java/ru/netimen/hitch_hikingstats/GoFragmentTest.kt
+++ app/src/androidTest/java/ru/netimen/hitch_hikingstats/GoFragmentTest.kt
@@ -1,13 +1,18 @@
 package ru.netimen.hitch_hikingstats
 
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import org.hamcrest.CoreMatchers.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import android.support.test.espresso.Espresso.*
-import android.support.test.espresso.matcher.ViewMatchers.*
-import android.support.test.espresso.assertion.ViewAssertions.*
+import ru.netimen.hitch_hikingstats.domain.GoState
 
 /**
  * Copyright (c) 2016 Bookmate.
@@ -17,13 +22,41 @@ import android.support.test.espresso.assertion.ViewAssertions.*
  * Author: Dmitry Gordeev @dreamindustries.co>
  * Date:   21.03.16
  */
+
+private inline fun <reified T: Fragment>getFragmentByClass(activity : AppCompatActivity) = activity.supportFragmentManager.fragments.find { it is T } as T
+
 @RunWith(AndroidJUnit4::class)
 class GoFragmentTest {
     @get:Rule
     val activityRule = ActivityTestRule(MainActivity::class.java)
 
+    val fragment by lazy { getFragmentByClass<GoFragment>(activityRule.activity) }
+
     @Test
     fun testStateDisplayed() {
-        onView(withText("Hello world!")).check(matches(isDisplayed()!!));
+        fragment.showState(GoState.Idle())
+        waitButton().check(matches(isDisplayed()));
+        rideButton().check(matches(isDisplayed()));
+        stopButton().check(matches(not(isDisplayed())));
+        onView(withText(containsString(R.string.idle))).check(matches(isDisplayed()))
+
+        fragment.showState(GoState.Waiting())
+        waitButton().check(matches(not(isDisplayed())));
+        rideButton().check(matches(isDisplayed()));
+        stopButton().check(matches(isDisplayed()));
+        onView(withText(containsString(R.string.waiting))).check(matches(isDisplayed()))
+
+        fragment.showState(GoState.Riding("Toyota", 3))
+        waitButton().check(matches(not(isDisplayed())));
+        rideButton().check(matches(isDisplayed()));
+        stopButton().check(matches(isDisplayed()));
+        onView(withText(containsString(R.string.riding))).check(matches(isDisplayed()))
     }
+
+    private fun containsString(id: Int) = containsString(getString(id))
+    private fun getString(id: Int) = fragment.resources.getString(id)
+
+    private fun waitButton() = onView(withText("Wait"))
+    private fun rideButton() = onView(withText("Ride"))
+    private fun stopButton() = onView(withText("Stop"))
 }
